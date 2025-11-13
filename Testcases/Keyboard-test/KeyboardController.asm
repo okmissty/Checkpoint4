fd09d7f0b88f8437c0e8c2aee21364e67ab440c9
Andy Quach <andy.quach@richmond.edu>
2025-11-12 22:34:36 -0500

    fixed commit

--- Testcases/Keyboard-test/KeyboardController.asm
+++ Testcases/Keyboard-test/KeyboardController.asm
@@ -3,9 +3,9 @@
 main:
   addi $sp,$0,-4096
 loop:
-  lw   $t0,-240($0)     # status
+  lw   $t0,-240($0)     # status to $t0  
   beq  $t0,$0,loop
-  lw   $t1,-236($0)     # char
+  lw   $t1,-236($0)     # char to $t1
   sw   $0,-240($0)      # pop
   sw   $t1,-256($0)     # echo
   j    loop
