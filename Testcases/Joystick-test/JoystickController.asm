ed014ac2759b57451b085a03b0a8a79a44a0cf3a
Andy Quach <andy.quach@richmond.edu>
2025-11-12 22:11:35 -0500

    fixed joystick

--- Testcases/Joystick-test/JoystickController.asm
+++ Testcases/Joystick-test/JoystickController.asm
@@ -1,14 +1,15 @@
 # X:-176 (0x3FFFF50), Y:-172 (0x3FFFF54)
 .text
 main:
-  addi $sp,$0,-4096
+    addi $sp,$0,-4096
+
 loop:
-  lw   $t0,-176($0)      # X
-  lw   $t1,-172($0)      # Y
-  sll  $t0,$t0,28        # mask low 4 bits without andi
-  srl  $t0,$t0,28
-  sll  $t1,$t1,28
-  srl  $t1,$t1,28
-  add  $s0,$s0,$t0
-  add  $s1,$s1,$t1
-  j    loop
+    lw   $t0,-176($zero)      # read X
+    andi $t0,$t0,0xF          # keep low 4 bits
+
+    lw   $t1,-172($zero)      # read Y
+    andi $t1,$t1,0xF          # keep low 4 bits
+
+    add  $s0,$s0,$t0          # s0 += X
+    add  $s1,$s1,$t1          # s1 += Y
+    j    loop
