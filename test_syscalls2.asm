3b10f32e573c29e61ce8a3767adbac12faac7e7f
Tyeon Ford <tyeon.ford@richmond.edu>
2025-11-17 21:18:26 -0500

    deleting extra test files

--- test_syscalls2.asm
+++
@@ -1,8 +0,0 @@
-# test_simple_syscall.asm
-# Simplest possible test - just call exit syscall
-
-.text
-    # Syscall 10 - Exit (should loop forever in Syscall10)
-    addi $v0, $zero, 10       # Set syscall code to 10
-    syscall                    # Call kernel
-    # Should never reach here - exit loops forever
